4c829e67b416c8dc7760d9f718ba724b3eb1d8db
Yuvraj Sharma <ys8484529@gmail.com>
2023-04-08 09:46:46 +0530

    Github workflow5

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   key_name      = "app-ssh-key"
-
+  
   tags = {
     Name = var.ec2_name
   }
